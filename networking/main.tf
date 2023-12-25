dd34563696a8a7be08c13de374e6772fa8666be4
Charles Jatto <jatto_charles@yahoo.com>
2023-12-25 14:39:11 -0500

    terraform-jenkins

--- networking/main.tf
+++ networking/main.tf
@@ -5,7 +5,7 @@ variable "eu_availability_zone" {}
 variable "cidr_private_subnet" {}
 
 output "dev_proj_1_vpc_id" {
-  value = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  value = aws_vpc.dev_proj_1_vpc_eu_west_1.id
 }
 
 output "dev_proj_1_public_subnets" {
@@ -17,7 +17,7 @@ output "public_subnet_cidr_block" {
 }
 
 # Setup VPC
-resource "aws_vpc" "dev_proj_1_vpc_eu_central_1" {
+resource "aws_vpc" "dev_proj_1_vpc_eu_west_1" {
   cidr_block = var.vpc_cidr
   tags = {
     Name = var.vpc_name
@@ -28,7 +28,7 @@ resource "aws_vpc" "dev_proj_1_vpc_eu_central_1" {
 # Setup public subnet
 resource "aws_subnet" "dev_proj_1_public_subnets" {
   count             = length(var.cidr_public_subnet)
-  vpc_id            = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  vpc_id            = aws_vpc.dev_proj_1_vpc_eu_west_1.id
   cidr_block        = element(var.cidr_public_subnet, count.index)
   availability_zone = element(var.eu_availability_zone, count.index)
 
@@ -40,7 +40,7 @@ resource "aws_subnet" "dev_proj_1_public_subnets" {
 # Setup private subnet
 resource "aws_subnet" "dev_proj_1_private_subnets" {
   count             = length(var.cidr_private_subnet)
-  vpc_id            = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  vpc_id            = aws_vpc.dev_proj_1_vpc_eu_west_1.id
   cidr_block        = element(var.cidr_private_subnet, count.index)
   availability_zone = element(var.eu_availability_zone, count.index)
 
@@ -51,7 +51,7 @@ resource "aws_subnet" "dev_proj_1_private_subnets" {
 
 # Setup Internet Gateway
 resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
-  vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  vpc_id = aws_vpc.dev_proj_1_vpc_eu_west_1.id
   tags = {
     Name = "dev-proj-1-igw"
   }
@@ -59,7 +59,7 @@ resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
 
 # Public Route Table
 resource "aws_route_table" "dev_proj_1_public_route_table" {
-  vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  vpc_id = aws_vpc.dev_proj_1_vpc_eu_west_1.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.dev_proj_1_public_internet_gateway.id
@@ -78,7 +78,7 @@ resource "aws_route_table_association" "dev_proj_1_public_rt_subnet_association"
 
 # Private Route Table
 resource "aws_route_table" "dev_proj_1_private_subnets" {
-  vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  vpc_id = aws_vpc.dev_proj_1_vpc_eu_west_1.id
   #depends_on = [aws_nat_gateway.nat_gateway]
   tags = {
     Name = "dev-proj-1-private-rt"
