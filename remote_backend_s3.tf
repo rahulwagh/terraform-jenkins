3827820a6bcb545889f564a004c78fcf110cd5f6
yourfavengineer <kendall8584@yahoo.com>
2024-01-24 17:33:27 -0800

    terraform init

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456"
-    key    = "devops-project-1/jenkins/terraform.tfstate"
+    bucket = "terraform-jenkinsapi-project-s3"
+    key    = "restapi-jenkins/terraform.tfstate"
     region = "eu-west-1"
   }
 }
